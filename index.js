a948a202e826ea19e7280ea8b6cab57ec274a35f
BankzAnls <chati3ank@hotmail.com>
2016-05-29 11:01:14 +0700

    fifteentime

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var token = 'EAAIwxSzOriIBACzvs1SsEZCq0tFmXHSDTs3smxARByXtVakr9MJ0rRuQ2RZAkxv8rO79CIcZCs0Kbds7ywTqsMkLQjXZAMT7ppDKcXbzc9P9ZCFpBaPcvN9TsXH8x274f5c5pM3IuxGZCcZCVnAKP36rtcnGC5Ykgdyg1EzVZAjNiAZDZD'
+var token = 'EAAIwxSzOriIBAKCK0l4wB8vMgfLuC9gChxW6PVhco3UM1ZAGAzpWmI48x3AveOjrUdN5IhZChE6ythZAMnesrswUcmzQugZCqoNk14oZCyzpOW7g6EwLko2LgomZCsAPZCzSlqMHZARZAUrKlckWW4YE7sC4RZCeR8qWnlcOQnZApe9UQZDZD'
 
 var app = express()
 
@@ -45,26 +45,26 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       var text = event.message.text
       console.log(text, sender)
-      var arrText = text.split(' ')
-      if (arrText[0] === 'sum') {
-        var sum = parseInt(arrText[1]) + parseInt(arrText[2])
+      var Textar = text.split(' ')
+      if (Textar[0] === 'sum') {
+        var sum = parseInt(Textar[1]) + parseInt(Textar[2])
         sendTextMessage(sender, sum + '')
-      } else if (arrText[0] === 'max') {
-        if (parseInt(arrText[1]) > parseInt(arrText[2])) {
-          sendTextMessage(sender, arrText[1])
+      } else if (Textar[0] === 'max') {
+        if (parseInt(Textar[1]) > parseInt(Textar[2])) {
+          sendTextMessage(sender, Textar[1])
         } else {
-          sendTextMessage(sender, arrText[2])
+          sendTextMessage(sender, Textar[2])
         }
-      } else if (arrText[0] === 'min') {
-        if (parseInt(arrText[1]) < parseInt(arrText[2])) {
-          sendTextMessage(sender, arrText[1])
+      } else if (Textar[0] === 'min') {
+        if (parseInt(Textar[1]) < parseInt(Textar[2])) {
+          sendTextMessage(sender, Textar[1])
         } else {
-          sendTextMessage(sender, arrText[2])
+          sendTextMessage(sender, Textar[2])
         }
-      } else if (arrText[0] === 'avg') {
+      } else if (Textar[0] === 'avg') {
         var avgSum = 0
         for (var b = 1; b < arrText.length; b++) {
-          avgSum = avgSum + parseInt(arrText[b])
+          avgSum = avgSum + parseInt(Textar[b])
         }
         var avg = avgSum / (arrText.length - 1)
         sendTextMessage(sender, avg)
